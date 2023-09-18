a33ff37bb104f4177811014e19347858e34dfea6
InbalGam <inbalgam@post.bgu.ac.il>
2023-09-18 21:13:15 +0300

    added refresh button

--- frontend/src/Components/GamePlay.js
+++ frontend/src/Components/GamePlay.js
@@ -4,6 +4,9 @@ import { useNavigate } from 'react-router-dom';
 import { performShot } from '../Api';
 import styles from './Styles/GamePlay.css';
 import Chat from './Chat';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import Fab from '@mui/material/Fab';
+import CircularProgress from '@mui/material/CircularProgress';
 
 
 function GamePlay(props) {
@@ -34,23 +37,27 @@ function GamePlay(props) {
 
 
     return (
-        <div className='container'>
-            <div className='error_msg'>
-                    {shotFail ? 'could not make this shot' : ''}
-                    {notMyTurn ? 'not your turn to make a shot' : ''}
-            </div>
-            <div className='player_board'>
-                <h3 className={props.myTurn ? 'player_turn' : ''}>{'You'}</h3>
-                <BoardGame dimension={props.dimension} shotsSent={props.shotsSent} shots={props.shotsSent} isLoading={isLoading} clicked={true} getIndexesData={getIndexesData} />
-            </div>
-            <div className='opponent_board'>
-                <h3 className={props.myTurn ? '' : 'opponent_turn'}>{props.opponent}</h3>
-                <BoardGame dimension={props.dimension} placedShips={props.placedShips} isLoading={isLoading} shots={props.shotsRecived} />
-            </div>
-            <div className='chat_container'>
-                <Chat />
-            </div>
-        </div>
+        <>
+            {isLoading ? <CircularProgress size={150} className='loader' /> :
+                <div className='container'>
+                    <Fab aria-label="refresh" onClick={props.getTheGameInfo}> <RefreshIcon /> </Fab>
+                    <div className='error_msg'>
+                        {shotFail ? 'could not make this shot' : ''}
+                        {notMyTurn ? 'not your turn to make a shot' : ''}
+                    </div>
+                    <div className='player_board'>
+                        <h3 className={props.myTurn ? 'player_turn' : ''}>{'You'}</h3>
+                        <BoardGame dimension={props.dimension} shotsSent={props.shotsSent} shots={props.shotsSent} isLoading={isLoading} clicked={true} getIndexesData={getIndexesData} />
+                    </div>
+                    <div className='opponent_board'>
+                        <h3 className={props.myTurn ? '' : 'opponent_turn'}>{props.opponent}</h3>
+                        <BoardGame dimension={props.dimension} placedShips={props.placedShips} isLoading={isLoading} shots={props.shotsRecived} />
+                    </div>
+                    <div className='chat_container'>
+                        <Chat />
+                    </div>
+                </div>}
+        </>
     );
 };
 
