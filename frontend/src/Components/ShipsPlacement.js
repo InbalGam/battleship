85d65e35fe0b512c0c2cd6fed74d0523c9430824
InbalGam <inbalgam@post.bgu.ac.il>
2023-09-18 11:30:43 +0300

    added clicked boolean

--- frontend/src/Components/ShipsPlacement.js
+++ frontend/src/Components/ShipsPlacement.js
@@ -166,7 +166,7 @@ function ShipsPlacement(props) {
                     {startGameFail ? 'Could not start game' : ''}
             </div>
             <div className='main_board'>
-                <BoardGame dimension={props.dimension} placedShips={props.placedShips} coloredCells={coloredCells} getIndexesData={getIndexesData} isLoading={isLoading} />
+                <BoardGame dimension={props.dimension} placedShips={props.placedShips} coloredCells={coloredCells} getIndexesData={getIndexesData} clicked={true} isLoading={isLoading} />
             </div>
         </div>
     );
