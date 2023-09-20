ba8f6e3ba853035f3a14a2d508b4320e19d6bf93
InbalGam <inbalgam@post.bgu.ac.il>
2023-09-20 12:26:21 +0300

    profile css

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -5,6 +5,8 @@ import EditIcon from '@mui/icons-material/Edit';
 import TextField from '@mui/material/TextField';
 import CircularProgress from '@mui/material/CircularProgress';
 import Alert from '@mui/material/Alert';
+import styles from './Styles/Profile.css';
+import Fab from '@mui/material/Fab';
 
 
 function Profile() {
@@ -72,21 +74,23 @@ function Profile() {
     return (
         <div>
             {isLoading ? <CircularProgress size={150} className='loader' /> :
-                <div>
+                <div className='profile_container'>
                     <p>Your username is {user.username}</p>
                     <div className='nicknameChange'>
                         {show ?
-                            <div>
+                            <div className='nickname_change_form'>
                                 <TextField id="outlined-basic" label="Nickname" variant="outlined" value={newNickname} onChange={handleUsernameChange} />
                                 <button type="submit" value="Submit" className="submitButton" onClick={submitNewNickname}>submit</button>
                             </div> : <p>Your nickname is {user.nickname}</p>}
-                        <button className='editIcon' onClick={showNicknameEdit}><EditIcon /></button>
+                        <Fab color="primary" aria-label="edit" onClick={showNicknameEdit} className='editIcon'> <EditIcon /> </Fab>
                     </div>
-                    <p>You current score is:</p>
-                    <div>
-                        <p>wins: {user.wins}</p>
-                        <p>loses: {user.loses}</p>
-                        <p>total: {user.wins - user.loses}</p>
+                    <div className='userScore'>
+                        <p>You current score is:</p>
+                        <div>
+                            <p className='wins'>wins: {user.wins}</p>
+                            <p className='loses'>Loses: {user.loses}</p>
+                            <p>Total: {user.wins - user.loses}</p>
+                        </div>
                     </div>
                     {updateFailed ? <Alert severity="warning">Could not update nickname</Alert> : ''}
                 </div>}
