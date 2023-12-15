8c730781f5f61b619e5897fb1dfb53e0174401ae
InbalGam <inbalgam@post.bgu.ac.il>
2023-12-15 21:02:09 +0200

    changed dir name

--- backend/hash.js
+++ backend/hash.js
@@ -1,28 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.comparePasswords = exports.passwordHash = void 0;
 const bcrypt = require("bcrypt");
-
-
 const passwordHash = async (password, saltRounds) => {
     try {
-      const salt = await bcrypt.genSalt(saltRounds);
-      return await bcrypt.hash(password, salt);
-    } catch (err) {
-      console.log(err);
+        const salt = await bcrypt.genSalt(saltRounds);
+        return await bcrypt.hash(password, salt);
+    }
+    catch (err) {
+        console.log(err);
     }
     return null;
 };
-
+exports.passwordHash = passwordHash;
 const comparePasswords = async (password, hash) => {
     try {
-      const matchFound = await bcrypt.compare(password, hash);
-      return matchFound;
-    } catch (err) {
-      console.log(err);
+        const matchFound = await bcrypt.compare(password, hash);
+        return matchFound;
+    }
+    catch (err) {
+        console.log(err);
     }
     return false;
-  };
-
-
-  module.exports = {
-    comparePasswords,
-    passwordHash
 };
+exports.comparePasswords = comparePasswords;
